49d7ca96dd027df0d0f3471c65890623d67fc976
Rhea <rheasing22@gmail.com>
2021-06-05 14:12:49 +0530

    fixed index.js

--- index.js
+++ index.js
@@ -1,140 +1,86 @@
 var readlineSync = require("readline-sync");
 var chalk = require('chalk');
+
+
+/*
+  function that displays the menu
+*/
+function displayMenu() {
+    var options = [
+        'Exit the program',
+        'Matrix Addition',
+        'Matrix Subtraction',
+        'Extract Matrix Diagonal',
+        'Matrix Transpose'
+    ];
+
+    options.forEach((elem, index) => {
+        console.log(chalk.blue(`${index}. ${elem}`));
+    });
+
+
+}
+
+/*
+checks if the var to exit the 
+do while loop is 'y' 'n' 'Y' or 'N' only 
+
+*/
+function isAnsValid(ans) {
+    if (ans == 'y' || ans == 'Y') {
+        return true;
+    }
+    else if (ans == 'n' || ans == 'N') {
+        return false;
+    }
+    else {
+        console.log(chalk.red.bold(`Wrong input:${ans}`));
+        console.log(chalk.red.bold(`enter either y or n`));
+        return true;
+    }
+}
+
 function main() {
+    console.clear(); // clear the menu the first time we run npm start
+
     do {
-        console.log("-------------------------");
-        var options = [
-            "Exit the Program",
-            "Matrix Addition",
-            "Matrix Subtraction",
-            "Matrix transpose"
-        ];
 
-        console.clear();
-        options.forEach((elem, index) => {
-            console.log(chalk.blue(`${index}. ${elem}`));
-        });
-        var flag = 1;
-        var correct_val = ['y', 'Y', 'n', 'N'];
+        var loop = false; // this var lets us stay or exit the do-while
+        displayMenu();
+        var choice = readlineSync.questionInt(chalk.green('Enter option: '));
 
-        var input;
-        input = readlineSync.questionInt(chalk.green("Enter Your Option :"));
 
-        switch (input) {
-
-            case 0:
-                console.log(chalk.yellow('Bye. Bye. Adios'));
+        switch (choice) {
+            case 0: console.log(chalk.magenta('Exiting'));
                 process.exit(0);
             case 1:
-                addition();
+                console.log('addition');
                 break;
             case 2:
-                substraction();
+                console.log('sub');
                 break;
             case 3:
-                diagonal();
+                console.log('diagonal');
                 break;
             case 4:
-                transpose();
+                console.log('transpose');
                 break;
             default:
-                flag = 0;
-                console.log(chalk.red("Bad Input"));
-
+                loop = true; // rerun menu 
+                console.clear();
+                console.log(chalk.red.bold(`Wrong input:${choice}`));
+                console.log(chalk.red.bold(`enter numbers between 0-4`));
 
 
         }
-
-
-        // if not bad input 
-        if (flag != 0) {
-            var take = readlineSync.question(chalk.green("Would You Like To Continue? (y or n)"));
-
-            while (correct_val.includes(take) === false) {
-                console.log(chalk.red('wrong input should be y or n '));
-                take = readlineSync.question(chalk.green("Would You Like To Continue? (y or n)"));
-
-                if (take === true) {
-                    break;
-                }
-
-            }
-
+        if (loop == true) {
+            continue; // this is to skip "do you want to continue?"" stmt
         }
-        else {
-            console.log(chalk.red("Bad Input"));
-            // bad input ask again 
-            take = 'y';
-        }
+        var ans = readlineSync.question(chalk.yellow("Do you want to continue ? (y/n): "));
+        console.clear();
+        loop = isAnsValid(ans);
 
-
-
-
-
-    } while (take === 'y' || take === 'Y');
+    } while (loop);
 }
 
 main();
-
-
-function createMatrix() {
-    var array = [];
-    var rows = readlineSync.questionInt(chalk.green("How many rows do you want your matrix to have "));
-    var columns = readlineSync.questionInt(chalk.green("How many columns do you want your matrix to have "));
-
-
-    for (var i = 0; i < rows; i++) {
-        array.push([]);
-    }
-    console.log(array);
-
-    for (var i = 0; i < rows; i++) {
-        for (var j = 0; j < columns; j++) {
-            array[i][j] = readlineSync.questionInt(`value in ${i} and ${j} `);
-
-        }
-
-    }
-    console.log(array);
-
-    return array;
-}
-/*
-console.log('Matrix 1')
-var matrix1 = createMatrix();
-console.log('Matrix 2')
-var matrix2 = createMatrix();
-*/
-function addition() {
-    /*
-    var resultMatrix = []
-    for (i = 0; i < matrix1.length; i++) {
-        for (j = 0; j < matrix1[i].length; j++)
-            resultMatrix = matrix1[i][j] + matrix2[i][j];
-    }*/
-    console.log(chalk.magenta("addition"));
-}
-
-function substraction() {
-    //createMatrix();
-    console.log(chalk.magenta("subtraction"));
-
-}
-
-function diagonal() {
-    //createMatrix();
-    console.log(chalk.magenta("diagonal"));
-
-}
-
-function transpose() {
-    //createMatrix();
-    console.log(chalk.magenta("transpose"));
-
-}
-
-
-
-
-
-
